9f6cb7170beacbaebec5e815575c56c366ce3a15
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-14 10:11:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace File_Handling
+{ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+    
+            Console.WriteLine("File Handling......!");
+            FileStream fs = new FileStream("C:\\Users\\Anusha.Sana\\OneDrive - Trianz\\Desktop\\My Details.txt", FileMode.Open,FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            Console.WriteLine("We can Read and Display from file");
+
+            string str = sr.ReadLine();
+            while(str!= null)
+            {
+                Console.WriteLine(str);
+                str = sr.ReadLine();
+            }
+
+          
+            sr.Close();
+
+
+            FileStream fWrite = new FileStream("C:\\Users\\Anusha.Sana\\OneDrive - Trianz\\Desktop\\My Details.txt", FileMode.Append);
+            StreamWriter swrite = new StreamWriter(fWrite);
+            Console.WriteLine("Writing content to file.........\n");
+            string content = Console.ReadLine();
+
+            //string content = Console.ReadLine();
+            swrite.Write(content);
+            swrite.Flush();
+            fWrite.Flush();
+
+            fWrite.Close();
+        }
+    }
+}
